40b4f4546c712a5f3be07252016f4a130b6349b6
Vyom Raturi <vyomraturi2004@gmail.com>
2024-06-04 16:04:31 +0530

    testing

--- test/Escrow.js
+++ test/Escrow.js
@@ -2,9 +2,14 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe('Escrow', () => {
-
-})
+  it('saves the addresses', async () => {
+    const RealEstate = await ethers.getContractFactory('RealEstate');
+    const realEstate = await RealEstate.deploy();
+    await realEstate.deployed();
+    console.log('RealEstate deployed to:', realEstate.address);
+  });
+});
