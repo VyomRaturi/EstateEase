cd0187a09b8d17b5172ea7edf0fe6387c0edf4e1
Vyom Raturi <vyomraturi2004@gmail.com>
2024-06-04 15:50:00 +0530

    escrow

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public lender;
+    address public inspector;
 
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _lender,
+        address _inspector
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        lender = _lender;
+        inspector = _inspector;
+    }
 }
