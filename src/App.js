5b5fcf9f5795c5d32edfb7c524700ff88786f73c
Vyom Raturi <vyomraturi2004@gmail.com>
2024-06-05 16:16:19 +0530

    connections

--- src/App.js
+++ src/App.js
@@ -14,10 +14,32 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const [homes, setHomes] = useState([])
+  const [home, setHome] = useState({})
+  const [toggle, setToggle] = useState(false);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, []);
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
