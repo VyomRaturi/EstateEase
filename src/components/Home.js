4f44d9ac6ed9651a0768730abead69ec5029250d
Vyom Raturi <vyomraturi2004@gmail.com>
2024-06-05 21:52:48 +0530

    handlers

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 import close from '../assets/close.svg';
 
-const Home = ({ home, provider, escrow, togglePop }) => {
+const Home = ({ home, provider, account, escrow, togglePop }) => {
   const [hasBought, setHasBought] = useState(false)
   const [hasLended, setHasLended] = useState(false)
   const [hasInspected, setHasInspected] = useState(false)
@@ -55,6 +55,59 @@ const Home = ({ home, provider, escrow, togglePop }) => {
 
     const owner = await escrow.buyer(home.id)
     setOwner(owner)
+  }
+
+  const buyHandler = async () => {
+    const escrowAmount = await escrow.escrowAmount(home.id)
+    const signer = await provider.getSigner()
+
+    // Buyer deposit earnest
+    let transaction = await escrow.connect(signer).depositEarnest(home.id, { value: escrowAmount })
+    await transaction.wait()
+
+    // Buyer approves...
+    transaction = await escrow.connect(signer).approveSale(home.id)
+    await transaction.wait()
+
+    setHasBought(true)
+  }
+
+  const inspectHandler = async () => {
+    const signer = await provider.getSigner()
+
+    // Inspector updates status
+    const transaction = await escrow.connect(signer).updateInspectionStatus(home.id, true)
+    await transaction.wait()
+
+    setHasInspected(true)
+  }
+
+  const lendHandler = async () => {
+    const signer = await provider.getSigner()
+
+    // Lender approves...
+    const transaction = await escrow.connect(signer).approveSale(home.id)
+    await transaction.wait()
+
+    // Lender sends funds to contract...
+    const lendAmount = (await escrow.purchasePrice(home.id) - await escrow.escrowAmount(home.id))
+    await signer.sendTransaction({ to: escrow.address, value: lendAmount.toString(), gasLimit: 60000 })
+
+    setHasLended(true)
+  }
+
+  const sellHandler = async () => {
+    const signer = await provider.getSigner()
+
+    // Seller approves...
+    let transaction = await escrow.connect(signer).approveSale(home.id)
+    await transaction.wait()
+
+    // Seller finalize...
+    transaction = await escrow.connect(signer).finalizeSale(home.id)
+    await transaction.wait()
+
+    setHasSold(true)
   }
 
   useEffect(() => {
